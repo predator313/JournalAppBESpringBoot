6d88ff75aafce64b4476da7125d1e3a56294fdb1
Aamir Ashraf <aamirnasa97@gmail.com>
2025-09-28 00:02:43 +0530

    health check method implemented

--- src/main/java/com/example/journalApp/JournalApplication.java
+++ src/main/java/com/example/journalApp/JournalApplication.java
@@ -1,13 +1,13 @@
-package com.example.journelApp;
+package com.example.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JournelApplication {
+public class JournalApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JournelApplication.class, args);
+		SpringApplication.run(JournalApplication.class, args);
 	}
 
 }
